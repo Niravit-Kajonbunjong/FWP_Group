4aa904094b70e1f35dd50bd15e6d5bab4a0fe527
RTha66 <66070175@kmitl.ac.th>
2026-02-28 20:00:17 +0700

    เพิ่ม menubar

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 
 // Connect to SQLite database
-// let db = new sqlite3.Database('your-db-filename.db', (err) => {    
-//   if (err) {
-//       return console.error(err.message);
-//   }
-//   console.log('Connected to the SQlite database.');
-// });
+let db = new sqlite3.Database('db.db', (err) => {    
+  if (err) {
+      return console.error(err.message);
+  }
+  console.log('Connected to the SQlite database.');
+});
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -22,8 +22,16 @@ app.set('view engine', 'ejs');
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// app.get("/", (req, res) => {
+//   res.render("login"); 
+// });
+
+// app.get("/", (req, res) => {
+//   res.render("teacherHome"); 
+// });
+
 app.get("/", (req, res) => {
-  res.render("login"); 
+  res.render("table"); 
 });
 
 app.listen(port, () => {
